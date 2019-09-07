fb67fc3049250c05bef55f2ed7734f66725e8c9e
Luciana Seljak <lseljak92@gmail.com>
2019-09-07 10:48:56 -0700

    Declare HashMap and modify operators functions

--- calculator/src/main/java/edu/csc413/calculator/operators/Operator.java
+++ calculator/src/main/java/edu/csc413/calculator/operators/Operator.java
@@ -1,22 +1,26 @@
 package edu.csc413.calculator.operators;
-
 import edu.csc413.calculator.evaluator.Operand;
-
 import java.util.HashMap;
 
 public abstract class Operator {
-    // The Operator class should contain an instance of a HashMap.
-    // The keys of the map will be the tokens we're interested in,
-    // and values will be instances of Operator.
-    // ALL subclasses of operator MUST be in their own file.
-    // 
-    // Where does this declaration go?
-    // What should its access level be?
-    // Class or instance variable?
-    // Is this the right declaration?
-    // HashMap operators = new HashMap();
-    // operators.put( "+", new AdditionOperator() );
-    // operators.put( "-", new SubtractionOperator() );
+    /**
+     * declaration of a private static operators HashMap
+     * private: in order to control the access
+     * static: so that every class gets a share
+     */
+    private static HashMap<String, Operator> operators = new HashMap<>();
+
+    /**
+     * program automatically initializes HashMap
+     * static block only runs once
+     */
+    static{
+        operators.put("+", new AddOperator());
+        operators.put("-", new SubtractOperator());
+        operators.put("*", new MultiplyOperator());
+        operators.put("/", new DivideOperator());
+        operators.put("^", new PowerOperator());
+    }
 
     /**
      * used to get the priority of an operator
@@ -25,19 +29,15 @@ public abstract class Operator {
      */
     public abstract int priority();
 
-    public Operand execute(Operand op1, Operand op2) {
-        return null;
-    }
-
+    public abstract Operand execute(Operand op1, Operand op2);
 
     /**
      * determines if a given token is a valid operator.
-     * please do your best to avoid static checks
-     * for example token.equals("+") and so on.
-     * Think about what happens if we add more operators.
+     * @param token key of the operator that need to be checked
+     * using containsKey method which provides constant-time performance
      */
     public static boolean check(String token) {
-        return false;
+        return operators.containsKey(token);
     }
 
     /**
@@ -49,6 +49,8 @@ public abstract class Operator {
      * @return reference to a Operator instance.
      */
     public static Operator getOperator(String token) {
+        if(operators.containsKey(token))
+            return operators.get(token);
         return null;
     }
 }
